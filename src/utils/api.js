c09291433800f1cbaadc2ee5a178488eee94cba4
Mohammed <sajidcurious9@gmail.com>
2023-10-25 21:37:20 +0530

    added rapid api

---
+++ src/utils/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+  params: {
+    hl: "en",
+    gl: "US",
+  },
+  headers: {
+    "X-RapidAPI-Key": REACT_APP_YOUTUBE_API_KEY,
+    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  },
+};
+
+const fetchDataFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+  return data;
+};
