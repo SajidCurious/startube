ef787b46fe5ddfde94f26cd159a71e7784a5a657
Mohammed <sajidcurious9@gmail.com>
2023-10-29 00:41:07 +0530

    imported necessary files and icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,18 @@
 import React from "react";
+import React, { useContext, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+
+import ytLogo from "../images/yt-logo.png";
+import ytLogoMobile from "../images/yt-logo-mobile.png";
+
+import { SlMenu } from "react-icons/sl";
+import { IoIosSearch } from "react-icons/io";
+import { RiVideoAddLine } from "react-icons/ri";
+import { FiBell } from "react-icons/fi";
+import { CgClose } from "react-icons/cg";
+
+import { Context } from "../context/contextApi";
+import Loader from "../shared/loader";
 
 const Header = () => {
   return <div>Header</div>;
