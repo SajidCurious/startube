12adf34a1ec44e123130fdb55af3dc9b99c01198
Mohammed <sajidcurious9@gmail.com>
2023-10-29 00:28:40 +0530

    added routes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,26 @@
-import { AppContext } from "./context/contextApi";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
 import SearchResult from "./components/SearchResult";
 import VideoDetails from "./components/VideoDetails";
 import Feed from "./components/feed";
+import { AppContext } from "./context/contextApi";
+
 function App() {
   return (
     <AppContext>
-      <div className="text-9xl">ajfalkfaklsjfafkj</div>
+      <BrowserRouter>
+        <div className="flex flex-col h-full">
+          <Header />
+          <Routes>
+            <Route path="/" exact element={<Feed />} />
+            <Route
+              path="/searchResult/:searchQuery"
+              element={<SearchResult />}
+            />
+            <Route path="/video/:id" element={<VideoDetails />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </AppContext>
   );
 }
