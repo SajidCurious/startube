420853cfc441dfe680be8e3acfa024871e60d5cc
Mohammed <sajidcurious9@gmail.com>
2023-10-29 02:17:41 +0530

    commented the api call

--- src/context/contextApi.jsx
+++ src/context/contextApi.jsx
@@ -9,18 +9,18 @@ export const AppContext = (props) => {
   const [selectedCategory, setSelectedCategory] = useState("New");
   const [mobileMenu, setMobileMenu] = useState(false);
 
-  useEffect(() => {
-    fetchSelectedCategoryData(selectedCategory);
-  }, [selectedCategory]);
+  // useEffect(() => {
+  //   fetchSelectedCategoryData(selectedCategory);
+  // }, [selectedCategory]);
 
-  const fetchSelectedCategoryData = (query) => {
-    setLoading(true);
-    fetchDataFromAPI(`search/?q=${query}`).then(({ contents }) => {
-      console.log(contents);
-      setSearchResults(contents);
-      setLoading(false);
-    });
-  };
+  // const fetchSelectedCategoryData = (query) => {
+  //   setLoading(true);
+  //   fetchDataFromAPI(`search/?q=${query}`).then(({ contents }) => {
+  //     console.log(contents);
+  //     setSearchResults(contents);
+  //     setLoading(false);
+  //   });
+  // };
 
   return (
     <Context.Provider
