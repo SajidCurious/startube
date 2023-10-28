3557ca83f76dda0763bb8ba25c88c26756a10a87
Mohammed <sajidcurious9@gmail.com>
2023-10-28 23:33:56 +0530

    renamed file

---
+++ src/context/contextApi.jsx
@@ -0,0 +1,41 @@
+import React, { createContext, useState, useEffect } from "react";
+
+import { fetchDataFromAPI } from "../utils/api";
+export const Context = createContext();
+
+export const AppContext = (props) => {
+  const [loading, setLoading] = useState(false);
+  const [searchResults, setSearchResults] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [mobileMenu, setMobileMenu] = useState(false);
+
+  useEffect(() => {
+    fetchSelectedCategoryData(selectedCategory);
+  }, [selectedCategory]);
+
+  const fetchSelectedCategoryData = (query) => {
+    setLoading(true);
+    fetchDataFromAPI(`search/?q=${query}`).then((res) => {
+      console.log(res);
+      setSearchResults(res);
+      setLoading(false);
+    });
+  };
+
+  return (
+    <Context.Provider
+      value={{
+        loading,
+        setLoading,
+        searchResults,
+        setSearchResults,
+        selectedCategory,
+        setSelectedCategory,
+        mobileMenu,
+        setMobileMenu,
+      }}
+    >
+      {props.children}
+    </Context.Provider>
+  );
+};
