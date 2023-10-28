3bd4b3e9e4f90d708865bf086f92802e1ebce21c
Mohammed <sajidcurious9@gmail.com>
2023-10-29 01:58:43 +0530

    added logos and input field in the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -37,6 +37,18 @@ const Header = () => {
     <div className="sticky top-0 z-10 flex flex-row items-center justify-between h-14 px-4 md:px-5 bg-white dark:bg-black">
       {loading && <Loader />}
       <div className="flex h-5 items-center">
+        {pageName !== "video" && (
+          <div
+            className="flex md:hidden md:mr-6 cursor-pointer items-center justify-center h-10 w-10 rounded-full hover:bg-[#303030]/[0.6]"
+            onClick={mobileMenuToggle}
+          >
+            {mobileMenu ? (
+              <CgClose className="text-white text-xl" />
+            ) : (
+              <SlMenu className="text-white text-xl" />
+            )}
+          </div>
+        )}
         <Link to="/" className="flex h-5 items-center">
           <img
             className="h-full hidden dark:md:block"
@@ -54,6 +66,9 @@ const Header = () => {
           <input
             type="text"
             className="bg-transparent outline-none text-white pr-5 pl-5 md:pl-0 w-44 md:group-focus-within:pl-0 md:w-64 lg:w-[500px]"
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onKeyUp={searchQueryHandler}
+            value={searchQuery}
           />
         </div>
         <button className="w-[40px] md:w-[60px] h-8 md:h-10 flex items-center justify-center border border-l-0 border-[#303030] rounded-r-3xl bg-white/[0.1]">
