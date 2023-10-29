f869a377ee40b817dec61a4ba06ee0c178ea404b
Mohammed <sajidcurious9@gmail.com>
2023-10-29 21:45:26 +0530

    adding jsx in this component

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -4,8 +4,32 @@ import { useNavigate } from "react-router-dom";
 import LeftNavMenuItem from "./LeftNavMenuItem";
 import { categories } from "../utils/constants";
 import { Context } from "../context/contextApi";
+
 const LeftNav = () => {
-  return <div>LeftNav</div>;
+  const { selectedCategory, setSelectedCategory, mobileMenu } =
+    useContext(Context);
+
+  return (
+    <div
+      className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
+        mobileMenu ? "translate-x-0" : ""
+      }`}
+    >
+      <div className="flex px-5 flex-col">
+        {categories.map((item) => {
+          return (
+            <React.Fragment>
+              <LeftNavMenuItem />
+            </React.Fragment>
+          );
+        })}
+        <hr className="my-5 border-white/[0.2]" />
+        <div className="text-white/[0.5] text-[12px]">
+          Clone by: JS Dev Hindi
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LeftNav;
