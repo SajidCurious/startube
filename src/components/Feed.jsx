5caa7a69a2be955b74967be3c43be70e6475df7d
Mohammed <sajidcurious9@gmail.com>
2023-10-29 21:29:43 +0530

    added jsx and jumping to build the LeftNav.jsx because it is the part of the feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,7 +5,14 @@ import LeftNav from "./LeftNav";
 import VideoCard from "./VideoCard";
 
 const Feed = () => {
-  return <div>Feed</div>;
+  return (
+    <div className="flex flex-row h-[calc(100%-56px)]">
+      <LeftNav />
+      <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 p-5"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Feed;
