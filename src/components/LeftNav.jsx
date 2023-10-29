793670553983e4efea29785fd656c73929caf382
Mohammed <sajidcurious9@gmail.com>
2023-10-29 22:19:15 +0530

    solved the error

--- src/components/LeftNav.jsx
+++ src/components/LeftNav.jsx
@@ -9,6 +9,8 @@ const LeftNav = () => {
   const { selectedCategory, setSelectedCategory, mobileMenu } =
     useContext(Context);
 
+  const navigate = useNavigate();
+
   return (
     <div
       className={`md:block w-[240px] overflow-y-auto h-full py-4 bg-black absolute md:relative z-10 translate-x-[-240px] md:translate-x-0 transition-all ${
@@ -18,8 +20,19 @@ const LeftNav = () => {
       <div className="flex px-5 flex-col">
         {categories.map((item) => {
           return (
-            <React.Fragment>
-              <LeftNavMenuItem />
+            <React.Fragment key={item.name}>
+              <LeftNavMenuItem
+                text={item.type === "home" ? "Home" : item.name}
+                icon={item.icon}
+                action={() => {
+                  clickHandler(item.name, item.type);
+                  navigate("/");
+                }}
+                className={`${
+                  selectedCategory === item.name ? "bg-white/[0.15]" : ""
+                }`}
+              />
+              {item.divider && <hr className="my-5 border-white/[0.2]" />}
             </React.Fragment>
           );
         })}
