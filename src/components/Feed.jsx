e864b1972fc3460422ab005f46ca8a0a57b4f101
Mohammed <sajidcurious9@gmail.com>
2023-10-29 21:27:52 +0530

    building feed component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import VideoCard from "./VideoCard";
 
 const Feed = () => {
   return <div>Feed</div>;
