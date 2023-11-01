6ef63f14b7bd28f93ec3178af8ced4a1f1976fba
Mohammed <sajidcurious9@gmail.com>
2023-11-01 14:37:18 +0530

    adding functionality to the component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -3,9 +3,9 @@ import { abbreviateNumber } from "js-abbreviation-number";
 import { Link } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
 
-const VideoCard = () => {
+const VideoCard = ({ video }) => {
   return (
-    <Link to={``}>
+    <Link to={`/video/${video.videoId}`}>
       <div className="flex flex-col mb-8">
         <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
           <img className="h-full w-full object-cover" />
