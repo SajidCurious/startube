e00b2f9ded27cb4d19125b7d881cea129cc15c46
Mohammed <sajidcurious9@gmail.com>
2023-11-04 16:26:20 +0530

    building SearchResult component , it searches results as per the user search and present them

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,5 +1,10 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
 
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
+import LeftNav from "./LeftNav";
+import SearchResultVideoCard from "./SearchResultVideoCard";
 const SearchResult = () => {
   return <div>SearchResult</div>;
 };
