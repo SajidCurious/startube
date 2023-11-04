80df4d28373b43f657b804f58f451dec508fb113
Mohammed <sajidcurious9@gmail.com>
2023-11-04 15:52:33 +0530

    started building SuggestionVideos while watching the selected video

--- src/components/SuggestionVideoCard.jsx
+++ src/components/SuggestionVideoCard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { abbreviateNumber } from "js-abbreviation-number";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
+import VideoLength from "../shared/videoLength";
 
 const SuggestionVideoCard = () => {
   return <div>SuggestionVideoCard</div>;
