f3c62ffabee2d7fac49fc510c0b367a3fcb9da6b
Mohammed <sajidcurious9@gmail.com>
2023-11-04 14:31:50 +0530

    adding functionality

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -2,13 +2,17 @@ import React from "react";
 import { abbreviateNumber } from "js-abbreviation-number";
 import { Link } from "react-router-dom";
 import { BsFillCheckCircleFill } from "react-icons/bs";
+import VideoLength from "../shared/videoLength";
 
 const VideoCard = ({ video }) => {
   return (
     <Link to={`/video/${video.videoId}`}>
       <div className="flex flex-col mb-8">
         <div className="relative h-48 md:h-40 md:rounded-xl overflow-hidden">
-          <img className="h-full w-full object-cover" />
+          <img
+            className="h-full w-full object-cover"
+            src={video?.thumbnails?.[0]?.url}
+          />
         </div>
         <div className="flex text-white mt-3">
           <div className="flex items-start">
--- src/shared/videoLength.jsx
+++ src/shared/videoLength.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import moment from "moment";
 
-const videoLength = () => {
-  return <div>videoLength</div>;
+const VideoLength = () => {
+  return (
+    <span className="absolute bottom-2 right-2 bg-black py-1 px-2 text-white text-xs rounded-md"></span>
+  );
 };
 
-export default videoLength;
+export default VideoLength;
