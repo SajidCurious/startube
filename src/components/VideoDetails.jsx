0b76d26475822d4bf1b44e7a234da388542228e6
Mohammed <sajidcurious9@gmail.com>
2023-11-04 15:10:28 +0530

    Imported necessary items

--- src/components/VideoDetails.jsx
+++ src/components/VideoDetails.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import React, { useState, useEffect, useContext } from "react";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player/youtube";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+import { AiOutlineLike } from "react-icons/ai";
+import { abbreviateNumber } from "js-abbreviation-number";
+
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
+import SuggestionVideoCard from "./SuggestionVideoCard";
 
 const VideoDetails = () => {
   return <div>VideoDetails</div>;
