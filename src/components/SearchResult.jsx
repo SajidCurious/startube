a2178f42571073ec533116c65a67c8d383fb0328
Mohammed <sajidcurious9@gmail.com>
2023-11-05 16:45:51 +0530

    Imported required

--- src/components/SearchResult.jsx
+++ src/components/SearchResult.jsx
@@ -1,10 +1,11 @@
 import React, { useState, useEffect, useContext } from "react";
 import { useParams } from "react-router-dom";
 
-import { fetchDataFromApi } from "../utils/api";
+import { fetchDataFromAPI } from "../utils/api";
 import { Context } from "../context/contextApi";
 import LeftNav from "./LeftNav";
 import SearchResultVideoCard from "./SearchResultVideoCard";
+
 const SearchResult = () => {
   return <div>SearchResult</div>;
 };
