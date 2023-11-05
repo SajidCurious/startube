d2c01217dcfb3d436c6386f3ae0cbf60da3106df
Mohammed <sajidcurious9@gmail.com>
2023-11-05 17:02:54 +0530

    building SearchResultVideoCard.jsx and imported required items

--- src/components/SearchResultVideoCard.jsx
+++ src/components/SearchResultVideoCard.jsx
@@ -1,4 +1,9 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { abbreviateNumber } from "js-abbreviation-number";
+import { BsFillCheckCircleFill } from "react-icons/bs";
+
+import VideoLength from "../shared/videoLength";
 
 const SearchResultVideoCard = () => {
   return <div>SearchResultVideoCard</div>;
